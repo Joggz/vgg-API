03f61a8d06f908d12962e17525d534719d321926
Idris <idrisaboladeo@gmail.com>
2020-02-29 02:34:08 +0100

    sets up development env, install package and sets up DB

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init DB
+db = SQLAlchemy(app)
+
+#init ma
+ma = Marshmallow(app)
+
+#Run server
+if __name__ == '__main__':
+  app.run(debug=True)
